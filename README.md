6986c02865ce04063285f7f7cbd439a8b1e35f08
Malte Reißig <malte@mikromedia.de>
2014-12-07 00:19:28 +0100

    Adapted to be compatible with DM 4.4

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This plugin provides service provides a simple, AJAX and HTML-Template based, `l
 This plugin adds:
 *    An `E-Mail Address` to each `User Account`   
      (Aggregation Definition, with a _one_ to _one_ relation)
-*    A `Sign-up`-link appears next to the `Login`-button in the DeepaMehta 4 Webclient
+*    A `Sign-up`-link next to the `Login`-button in the DeepaMehta 4 Webclient
 
 The special features of the `registration` dialog is comprised of:
 *    Username existence check
@@ -26,13 +26,13 @@ https://github.com/jri/deepamehta
 
 To be able to install this module you first and additionally have to install the following DeepaMehta 4 Plugins.
 
-*    `dm43-webactivator-0.4.2`-Bundle - [Source Code](https://github.com/jri/dm4-webactivator), [Nightly Build](http://download.deepamehta.de/nightly/)
+*    `dm44-webactivator-0.4.4`-Bundle - [Source Code](https://github.com/jri/dm4-webactivator), [Nightly Build](http://download.deepamehta.de/nightly/)
 
 ## Download & Installation
 
-You can find the latest `dm42-sign-up-X.Y.Z.jar-`-Bundle to download at [http://download.deepamehta.de/nightly/](http://download.deepamehta.de/nightly/).
+You can find the latest `dm44-sign-up-X.Y.Z.jar-`-Bundle to download at [http://download.deepamehta.de/nightly/](http://download.deepamehta.de/nightly/).
 
-After downloading the bundle-file, place it in the `bundles` folder of your DeepaMehta installation and restart DeepaMehta.
+After downloading the two bundle-files (webactivator and sign-up), place those in the `bundles` folder of your DeepaMehta installation and restart DeepaMehta.
 
 ## Licensed under the GPL License 3.0
 
@@ -40,9 +40,9 @@ GPL v3 - https://www.gnu.org/licenses/gpl.html
 
 # Version History
 
-1.0-SNAPSHOT, UPCOMING
+1.0, November 17, 2014
 
-- compatible with 4.4-SNAPSHOT
+- compatible with 4.4 releaase
 - basically working
 
 -------------------------------
--- src/main/java/org/deepamehta/plugins/signup/SignupService.java
+++ src/main/java/org/deepamehta/plugins/signup/SignupService.java
@@ -1,17 +1,18 @@
 package org.deepamehta.plugins.signup;
 
 import com.sun.jersey.api.view.Viewable;
+import de.deepamehta.core.Association;
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.model.*;
-import de.deepamehta.core.service.ClientState;
-import de.deepamehta.core.service.PluginService;
-import de.deepamehta.core.service.annotation.ConsumesService;
+import de.deepamehta.core.service.Inject;
+import de.deepamehta.core.service.Transactional;
 import de.deepamehta.plugins.accesscontrol.model.ACLEntry;
 import de.deepamehta.plugins.accesscontrol.model.AccessControlList;
 import de.deepamehta.plugins.accesscontrol.model.Operation;
 import de.deepamehta.plugins.accesscontrol.model.UserRole;
 import de.deepamehta.plugins.accesscontrol.service.AccessControlService;
 import de.deepamehta.plugins.webactivator.WebActivatorPlugin;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -25,10 +26,10 @@ import org.codehaus.jettison.json.JSONObject;
  * /sign-up => signup-form
  * /ok      => info-page after account creation
  *
- * @name org.deepamehta-sign-up
- * @website https://github.com/mukil/org.deepamehta-sign-up
- * @version 1.0.0-SNAPSHOT
- * @author malt
+ * @name dm4-sign-up
+ * @website https://github.com/mukil/dm4-sign-up
+ * @version 1.0.0
+ * @author <a href="mailto:malte@mikromedia.de">Malte Reissig</a>;
  */
 
 @Path("/")
@@ -36,8 +37,8 @@ public class SignupService extends WebActivatorPlugin {
 
     private static Logger log = Logger.getLogger(SignupService.class.getName());
 
-	/** @see also @de.deepamehta.plugins.accesscontrol.model.Credentials */
-	private static final String ENCRYPTED_PASSWORD_PREFIX = "-SHA256-";
+    /** @see also @de.deepamehta.plugins.accesscontrol.model.Credentials */
+    private static final String ENCRYPTED_PASSWORD_PREFIX = "-SHA256-";
 
     public final static String USERNAME_TYPE_URI = "dm4.accesscontrol.username";
     public final static String USER_ACCOUNT_TYPE_URI = "dm4.accesscontrol.user_account";
@@ -47,27 +48,29 @@ public class SignupService extends WebActivatorPlugin {
 
     public final static String WS_DM_DEFAULT_URI = "de.workspaces.deepamehta";
     public final static String WS_WIKIDATA_URI = "org.deepamehta.workspaces.wikidata";
+    
+    final String ADMINISTRATOR_USERNAME = "admin";
 
-    private AccessControlService acService = null;
+    @Inject
+    private AccessControlService acService;
 
     @Override
     public void init() {
         initTemplateEngine();
-	}
+    }
 
     /** Plugin Service Implementation */
 
     @GET
     @Path("/sign-up/check/{username}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getUsernameAvailability(@PathParam("username") String username,
-			@HeaderParam("Cookie") ClientState clientState) {
+    public String getUsernameAvailability(@PathParam("username") String username) {
 		JSONObject response = new JSONObject();
         try {
-			response.put("isAvailable", true);
-			if (!isUsernameAvailable(username, clientState)) response.put("isAvailable", false);
-			//
-			return response.toString();
+            response.put("isAvailable", true);
+            if (!isUsernameAvailable(username)) response.put("isAvailable", false);
+            //
+            return response.toString();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -75,84 +78,74 @@ public class SignupService extends WebActivatorPlugin {
 
     @GET
     @Path("/sign-up/create/{username}/{pass-one}/{mailbox}")
+    @Transactional
     public String createSimpleUserAccount(@PathParam("username") String username, @PathParam("mailbox") String mailbox,
             @PathParam("pass-one") String password) {
-
         try {
-			if (!isUsernameAvailable(username, null)) throw new WebApplicationException(412);
-			if (!isPasswordGood(password, null)) throw new WebApplicationException(412);
-            log.info("Setting up new \"User Account\" composite value model");
-			CompositeValueModel userAccount = new CompositeValueModel()
-					.put(USERNAME_TYPE_URI, username)
-					.put(USER_PASSWORD_TYPE_URI, password)
-					.put(MAILBOX_TYPE_URI, mailbox);
-			// ### set user account to "Blocked" until verified (introduce this in a new migration)
-			TopicModel userModel = new TopicModel(USER_ACCOUNT_TYPE_URI, userAccount);
-			Topic user = dms.createTopic(userModel, null);
+            if (!isUsernameAvailable(username)) throw new WebApplicationException(412);
+            if (!isPasswordGood(password)) throw new WebApplicationException(412);
+            log.fine("Setting up new \"User Account\" composite value model");
+            ChildTopicsModel userAccount = new ChildTopicsModel()
+                .put(USERNAME_TYPE_URI, username)
+                .put(USER_PASSWORD_TYPE_URI, password)
+                .put(MAILBOX_TYPE_URI, mailbox);
+            // ### set user account to "Blocked" until verified (introduce this in a new migration)
+            TopicModel userModel = new TopicModel(USER_ACCOUNT_TYPE_URI, userAccount);
+            Topic user = dms.createTopic(userModel);
             log.info("Created new \"User Account\" for " + username);
             acService.setACL(user, new AccessControlList( //
                             new ACLEntry(Operation.WRITE, UserRole.OWNER)));
             acService.setCreator(user, username);
             acService.setOwner(user, username);
-            log.warning("ACL-Properties are now set for " + username);
-            assignToDefaultWorkspace(user.getCompositeValue().getTopic(USERNAME_TYPE_URI));
-			return username;
+            // ### assign to custom workspace - make configurable
+            assignToDefaultWorkspace(user.getChildTopics().getTopic(USERNAME_TYPE_URI));
+            return username;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-
-
-    /** --- Implementing PluginService Interfaces to consume AccessControlService --- */
-
-    @Override
-    @ConsumesService(de.deepamehta.plugins.accesscontrol.service.AccessControlService.class)
-    public void serviceArrived(PluginService service) {
-        if (service instanceof AccessControlService) {
-            acService = (AccessControlService) service;
-        }
-    }
-
-    @Override
-    @ConsumesService(de.deepamehta.plugins.accesscontrol.service.AccessControlService.class)
-    public void serviceGone(PluginService service) {
-        if (service == acService) {
-            acService = null;
-        }
-    }
-
-
+    
 
     /** --- Private Helpers --- */
 
-	private boolean isUsernameAvailable(String username, ClientState clientState) {
-		// fixme: framework should also allow us to query case insensitve for a username
-		Topic userName = dms.getTopic(USERNAME_TYPE_URI, new SimpleValue(username), true);
-		return (userName == null) ? true : false;
-	}
+    private boolean isUsernameAvailable(String username) {
+        // fixme: framework should also allow us to query case insensitve for a username
+        Topic userName = dms.getTopic(USERNAME_TYPE_URI, new SimpleValue(username));
+        return (userName == null);
+    }
 
-	private boolean isPasswordGood(String password, ClientState clientState) {
-		// fixem: should be at least 13 chars long but actually we should work on implementing two-factor auth
-		return (password.length() >= 8) ? true : false;
-	}
+    private boolean isPasswordGood(String password) {
+        // fixem: should be at least 13 chars long but actually we should work on implementing two-factor auth
+        return (password.length() >= 8);
+    }
+    
+    private boolean associationExists(String edge_type, Topic item, Topic user) {
+        List<Association> results = dms.getAssociations(item.getId(), user.getId(), edge_type);
+        return (results.size() > 0);
+    }
 
     private void assignToDefaultWorkspace(Topic topic) {
-        Topic defaultWorkspace = dms.getTopic("uri", new SimpleValue(WS_DM_DEFAULT_URI), false);
-        dms.createAssociation(new AssociationModel("dm4.core.aggregation",
-            new TopicRoleModel(topic.getId(), "dm4.core.parent"),
-            new TopicRoleModel(defaultWorkspace.getId(), "dm4.core.child")
-        ), null);
+        Topic defaultWorkspace = dms.getTopic("uri", new SimpleValue(WS_DM_DEFAULT_URI));
+        if (!associationExists("dm4.core.aggregation", defaultWorkspace, topic)) {
+            dms.createAssociation(new AssociationModel("dm4.core.aggregation",
+                new TopicRoleModel(topic.getId(), "dm4.core.parent"),
+                new TopicRoleModel(defaultWorkspace.getId(), "dm4.core.child")
+            ));
+        } else {
+            log.warning("New User Account was already to default (\"DeepaMehta\") workspace "
+                + "(probably through already having a workspace cookie set?).");
+        }
     }
 
 
 
-	/** --- Sign-up Routes --- */
+    /** --- Sign-up Routes --- */
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public Viewable getLoginFormView() {
-		// fixme: use acl service to check if a session already exists and if so, redirect to dm-webclient directly
+        // fixme: use acl service to check if a session already exists and if so, redirect to dm-webclient directly
         return view("login");
     }
 
@@ -160,7 +153,7 @@ public class SignupService extends WebActivatorPlugin {
     @Path("/sign-up")
     @Produces(MediaType.TEXT_HTML)
     public Viewable getSignupFormView() {
-		// fixme: use acl service to check if a session already exists and if so, redirect to dm-webclient directly
+        // fixme: use acl service to check if a session already exists and if so, redirect to dm-webclient directly
         return view("sign-up");
     }
 
